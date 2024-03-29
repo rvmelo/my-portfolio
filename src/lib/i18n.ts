85309a451f0ee176882f26f9410470befb57b92d
rvmelo <robertomelo7@hotmail.com>
2024-03-29 11:50:35 -0300

    feat: add translation based in location

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,7 +1,8 @@
 import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
 import enTranslations from '../locale/english.json'
 import ptTranslations from '../locale/portuguese.json'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
 
 const resources = {
   en: {
@@ -12,13 +13,15 @@ const resources = {
   },
 }
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  //   fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-})
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
 
 export default i18n
